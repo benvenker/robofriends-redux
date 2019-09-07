61c6547afd92cd7fc7dea0850960a075dcb15033
Ben Venker <bvemails@gmail.com>
2019-09-07 11:50:48 -0700

    add aria label

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,15 +3,14 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-      <label for="searchbox"><input
+      <input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
         onChange={searchChange}
         id="searchbox"
+        aria-lable="Search"
       />
-      </label>
-
     </div>
   );
 }
