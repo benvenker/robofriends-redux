846f51840a8ad20d10bf153d5836792c13deb2b7
Ben Venker <bvemails@gmail.com>
2019-09-07 11:44:50 -0700

    try another label method

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,14 +3,15 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-      <label for="searchbox"></label>
-      <input
+      <label for="searchbox"><input
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
         onChange={searchChange}
         id="searchbox"
       />
+      </label>
+
     </div>
   );
 }
